73172569e50220d31f31708abf214303466df0c5
ebhi456 <ebinejar456@gmail.com>
2026-02-25 03:14:44 +0530

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "ebinejar_s3_1"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "ebinejar_s3_1"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "ebinejar_s3_2"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "ebinejar_s3_2"
     Environment = "dev"
   }
 }
