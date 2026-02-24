2f587c7dca0405d3cf5586cebf1ea6aa421458d9
ebhi456 <ebinejar456@gmail.com>
2026-02-25 05:01:00 +0530

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -7,12 +7,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "ebinejar_s3_2"
+    bucket = "ebinejar-s3-2"
     key    = "ecr/terraform.tfstate"
     region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
 }
+
 
 
